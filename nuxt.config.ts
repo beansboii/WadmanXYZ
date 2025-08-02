ada7d48a47735f05f58d7c0bf6530f6436d614b9
Wren <155106259+DisketteKitchen@users.noreply.github.com>
2025-08-02 15:20:19 -0600

    Fix stupid shit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/ui']
+  modules: ['@nuxt/image', '@nuxt/ui']
 })
